e544bc5d2d9bfa0f20abfe2397096eba81a6cd44
PavanKalyan <saladipavankalyan10@gmail.com>
2024-05-23 19:34:21 +0530

    1. Reverse the array method-1.

---
+++ Arrays/1. Reverse the array.py
@@ -0,0 +1,37 @@
+'''
+Array reverse or reverse a array means changing the 
+position of each number of the given array to its 
+opposite position from end, i.e. if a number is at 
+position 1 then its new position will be Array.length,
+similarly if a number is at position 2 then its 
+new position will be Array.length - 1, and so on.
+
+Given an array (or string), the task is to reverse the array/string.
+
+Examples:
+
+Input: original_array[] = {1, 2, 3} Output: array_reversed[] = {3, 2, 1}
+
+
+Input: original_array[] = {4, 5, 1, 2}
+Output: array_reversed[] = {2, 1, 5, 4}
+
+Table of Content
+
+1. Array Reverse Using an Extra Array (Non In-place):
+2. Array Reverse Using a Loop (In-place):
+3. Array Reverse Inbuilt Methods (Non In-place):
+4. Array Reverse Recursion (In-place or Non In-place):
+5. Array Reverse Stack (Non In-place):
+
+'''
+
+# 1. Array Reverse Using an Extra Array (Non In-place):
+
+def reverseArray(arr):
+    rev = []
+    for i in range(len(arr) - 1, -1, -1):
+        rev.append(arr[i])
+    return rev
+
+print(reverseArray([4, 5, 1, 2]))
