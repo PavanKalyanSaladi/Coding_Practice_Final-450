fd0e2668dbe5c9fd9751ada5c7279cf3b527608e
Pavan Kalyan <saladipavankalyan10@gmail.com>
2024-05-26 18:55:11 +0530

    3. Kth smallest element using Binary Search.

--- Arrays/3. Kth smallest element.py
+++ Arrays/3. Kth smallest element.py
@@ -40,7 +40,30 @@ def kthLeastElement(arr, k):
 
 arr = [7, 10, 4, 3, 20, 15]
 k = 4
-print(kthLeastElement(arr, k))
+print("Using array.sort() ", kthLeastElement(arr, k))
 
 
 # Approach 2
+'''
+Follow the given steps to solve the problem:
+
+Intialize low and high to minimum and maximum element of the array denoting the range within which the answer lies.
+Apply Binary Search on this range. 
+If the selected element by calculating mid has less than K elements lesser to it then increase the number that is low = mid + 1.
+Otherwise, Decrement the high pointer, i.e high = mid.
+The Binary Search will end when only one element remains in the answer space that would be the answer.
+'''
+
+def kthLeastBinarySearch(arr, k):
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[i]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr[k - 1]
+
+arr = [7, 10, 4, 3, 20, 15]
+k = 4
+print("Using Binary Search ", kthLeastElement(arr, k))
+
+
+# Approach 3
