fcfd12e07b59eb72582cffb8832beeccced84145
PavanKalyan <saladipavankalyan10@gmail.com>
2024-06-06 22:17:07 +0530

    3. Array Insertion.

--- Python/3. Arrays/2. 1D Array.py
+++ Python/3. Arrays/2. 1D Array.py
@@ -5,10 +5,18 @@ print(my_array)
 my_array1 = array.array('i', [1, 2, 3, 4])  # Only same type of elements can be stored
 print(my_array1)
 
-
 # numpy module  - Installation needs to be done before running
 import numpy as np
 np_array = np.array([],dtype=int)    # int type of empty array
 print(np_array)
 np_array1 = np.array([1, 2, 3, 4])
 print(np_array1)
+
+
+
+# INSERTION INTO ARRAY
+# Using array module
+my_array1 = array.array('i', [1, 2, 3, 4])
+print(my_array1)
+my_array1.insert(0, 6)
+print(my_array1)
