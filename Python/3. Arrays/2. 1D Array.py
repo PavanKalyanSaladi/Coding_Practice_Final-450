58ebc78bdb0af1fbe5e010dc745e28ac91c3e763
PavanKalyan <saladipavankalyan10@gmail.com>
2024-06-06 22:29:53 +0530

    3.2 Array Traversal.

--- Python/3. Arrays/2. 1D Array.py
+++ Python/3. Arrays/2. 1D Array.py
@@ -6,17 +6,26 @@ my_array1 = array.array('i', [1, 2, 3, 4])  # Only same type of elements can be 
 print(my_array1)
 
 # numpy module  - Installation needs to be done before running
-import numpy as np
-np_array = np.array([],dtype=int)    # int type of empty array
-print(np_array)
-np_array1 = np.array([1, 2, 3, 4])
-print(np_array1)
+# import numpy as np
+# np_array = np.array([],dtype=int)    # int type of empty array
+# print(np_array)
+# np_array1 = np.array([1, 2, 3, 4])
+# print(np_array1)
 
 
-
-# INSERTION INTO ARRAY
-# Using array module
+# INSERTION INTO ARRAY - Using array module
 my_array1 = array.array('i', [1, 2, 3, 4])
 print(my_array1)
 my_array1.insert(2, 6)
 print(my_array1)
+
+
+# TRAVERSAL OF AN ARRAY
+from array import *
+arr1 = array('i', [1, 2, 3, 4, 5])
+arr2 = array('d', [1.3, 1.5, 1.7])
+def traverArray(arr):
+    for i in arr:
+        print(i)
+traverArray(arr1)
+traverArray(arr2)
