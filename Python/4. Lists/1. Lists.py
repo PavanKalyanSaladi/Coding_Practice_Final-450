e2b7d627d571152cbab72e81d63ac2fd901b24df
PavanKalyan <saladipavankalyan10@gmail.com>
2024-06-13 23:19:54 +0530

    4.1 Lists Creation.

---
+++ Python/4. Lists/1. Lists.py
@@ -0,0 +1,22 @@
+'''
+List -  A list is a data structure that holds an ordered collection of items.
+List starts and ends with [ ]
+List elements doesn't need to have same type
+List of lists are allowed - ['spam', 1.0, [10, 20]]
+'''
+
+# Cretation of list
+integers = [1, 2, 3, 4]
+print(integers)
+
+shoppingList = ['Milk', 'Cheese', 'Banana']
+print(shoppingList)
+
+mixedList = [2.4, 'spam', 99]
+print(mixedList)
+
+nestedList = [1, 2, 'spam', 'me', [1, 1.0, '1']]
+print(nestedList)
+
+empty = []
+print(empty)
