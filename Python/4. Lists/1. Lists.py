f6876163e251182bec4721f10a881f72111256fd
PavanKalyan <saladipavankalyan10@gmail.com>
2024-06-13 23:29:40 +0530

    4.1 Lists Accessing & Traversing.

--- Python/4. Lists/1. Lists.py
+++ Python/4. Lists/1. Lists.py
@@ -3,6 +3,7 @@ List -  A list is a data structure that holds an ordered collection of items.
 List starts and ends with [ ]
 List elements doesn't need to have same type
 List of lists are allowed - ['spam', 1.0, [10, 20]]
+List indexing starts with 0 to N-1      N = Number of elements
 '''
 
 # Cretation of list
@@ -18,5 +19,19 @@ print(mixedList)
 nestedList = [1, 2, 'spam', 'me', [1, 1.0, '1']]
 print(nestedList)
 
-empty = []
+empty = []          # empty list prints - []
 print(empty)
+
+
+# Acessing / Traversing a List
+print(shoppingList[1])
+print(shoppingList[-1])                 # -1 indexing starts from back -> -3 -2 -1
+print('Bread' in shoppingList)
+
+for item in mixedList:                  # Iterate through list
+    print(item, end="_")
+print()
+for i in range(len(nestedList)):        # Iterate using index of list
+    print(nestedList[i], end=" ")
+print()
+print([item for item in integers])      # Single line iteration
